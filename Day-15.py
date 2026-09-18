d15bea752774e2db4be765b9aa39b5b2b774b569
pandakokoro <amirkingnaro@gmail.com>
2026-09-18 08:26:09 +0530

    Add turtle graphics pattern drawing in Day-15.py
    This script uses the turtle graphics library to create a complex pattern with loops and angles.

---
+++ Day-15.py
@@ -0,0 +1,34 @@
+import turtle as t
+import colorsys 
+s = t.Screen()
+s.bgcolor("grey")
+
+
+
+for i in range(100):
+    t.speed(100)
+    
+    
+    t.left(125)
+    t.forward(100)
+    
+    t.right(35)
+    t.backward(20)
+    t.right(25)
+    t.backward(50)
+    for i in range(2):
+        
+        t.left(125)
+        t.forward(100)
+        
+        t.right(35)
+        t.backward(50)
+        
+        
+        t.left(115)
+        t.forward(100)
+        
+        t.right(25)
+        t.backward(50)
+    
+t.done()
